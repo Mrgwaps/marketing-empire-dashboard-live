40f96fa4f7d462486b5c886e74ba6eaaf9639496
Mrgwaps <mrgwaps@gmail.com>
2026-02-22 22:40:03 -0800

    FORCE CACHE BUST v5.1 - Add middleware + version tracking

--- middleware.js
+++ middleware.js
@@ -1,1 +1,18 @@
-{{ MIDDLEWARE_CONTENT }}
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+  const response = NextResponse.next()
+
+  // AGGRESSIVE CACHE BUSTING - Force browsers to never cache
+  response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0')
+  response.headers.set('Pragma', 'no-cache')
+  response.headers.set('Expires', '0')
+  response.headers.set('Surrogate-Control', 'no-store')
+  response.headers.set('X-Dashboard-Version', 'v5.1-' + Date.now())
+
+  return response
+}
+
+export const config = {
+  matcher: '/:path*',
+}
